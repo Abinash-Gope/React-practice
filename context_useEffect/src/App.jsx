35c3be596ef6e5fcdd8995b8da2ffebc94a6e0bd
Abinash Gope <abinashg797@gmail.com>
2026-07-25 23:53:42 +0530

    feat: integrate MyStore context for state management in App component

--- context_useEffect/src/App.jsx
+++ context_useEffect/src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import { MyStore } from "./context/MyContext";
 
 const App = () => {
   console.log("App rendering...");
 
+  let {count, setCount} = useContext(MyStore);
+
   return (
     <div>
       <h1>Hello - {count}</h1>
-      <button>Increment</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
       <Home />
       <About />
       <Contact />
