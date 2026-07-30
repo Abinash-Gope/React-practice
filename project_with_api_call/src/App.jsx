c95c39769d093908a83f9b977ebde2e492b6d4f2
Abinash Gope <abinashg797@gmail.com>
2026-07-30 22:53:52 +0530

    feat: implement CartScreen component and integrate cart functionality in App component

--- project_with_api_call/src/App.jsx
+++ project_with_api_call/src/App.jsx
@@ -2,10 +2,12 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import ProductCards from "./components/ProductCards";
+import CartScreen from "./pages/CartScreen";
 
 const App = () => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
-  const [productsData, setProductsData] = useState([])
+  const [productsData, setProductsData] = useState([]);
   const getProductsData = async () => {
     try {
       let res = await axios.get("https://fakestoreapi.com/products");
@@ -23,13 +25,17 @@ const App = () => {
     <div className="h-screen p-2 flex flex-col gap-4">
       <Navbar />
 
-    <div className="grid grid-cols-4 gap-4">
-      {
-        productsData.map((elem) => {
-          return <ProductCards key={elem.id} product={elem}/>
-        })
-      }
-    </div>
+      {isCartOpen ? (
+        <div>
+          <CartScreen setIsCartOpen={setIsCartOpen}/>
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-4">
+          {productsData.map((elem) => {
+            return <ProductCards key={elem.id} product={elem} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
---
+++ project_with_api_call/src/pages/CartScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartScreen = ({ setIsCartOpen }) => {
+  return (
+    <div className="h-screen">
+      <h1>This is cart section</h1>
+    </div>
+  );
+};
+
+export default CartScreen;
