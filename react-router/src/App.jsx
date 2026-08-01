e460db044d1f91e88b880501ba6130640fef61e3
Abinash Gope <abinashg797@gmail.com>
2026-08-01 22:59:06 +0530

    feat: add Home, About, and Contact components with basic structure

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <div>
-      <h1>hiii</h1>
+    <div className='h-screen p-2'>
+      <nav className='flex items-center justify-between mb-4'>
+        <h1>Logo</h1>
+        <div className='flex gap-10 items-center justify-between'>
+          <p>Home</p>
+          <p>About</p>
+          <p>Contact</p>
+        </div>
+        <button>Login</button>
+      </nav>
+      <Home />
+      <About />
+      <Contact />
     </div>
   )
 }
