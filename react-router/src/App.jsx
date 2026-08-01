ee3a5eb50c9e56ff0b5cc996b8e69853556f3e41
Abinash Gope <abinashg797@gmail.com>
2026-08-01 23:19:26 +0530

    feat: implement toggle functionality for navigation between Home, About, and Contact components

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import React, { useState } from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
+  const [toggle, setToggle] = useState("Home");
+
   return (
-    <div className='h-screen p-2'>
-      <nav className='flex items-center justify-between mb-4'>
+    <div className="h-screen p-2">
+      <nav className="flex items-center justify-between mb-4">
         <h1>Logo</h1>
-        <div className='flex gap-10 items-center justify-between'>
-          <p>Home</p>
-          <p>About</p>
-          <p>Contact</p>
+        <div className="flex gap-10 items-center justify-between">
+          <p className="cursor-pointer" onClick={() => setToggle("home")}>
+            Home
+          </p>
+          <p className="cursor-pointer" onClick={() => setToggle("about")}>
+            About
+          </p>
+          <p className="cursor-pointer" onClick={() => setToggle("contact")}>
+            Contact
+          </p>
         </div>
         <button>Login</button>
       </nav>
-      <Home />
-      <About />
-      <Contact />
+
+      <div>
+        {toggle === "home" && <Home />}
+        {toggle === "about" && <About />}
+        {toggle === "contact" && <Contact />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
