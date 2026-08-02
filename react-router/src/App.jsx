c375cad647149c36713a55744f0a18c10b2b2af2
Abinash Gope <abinashg797@gmail.com>
2026-08-02 23:30:13 +0530

    feat: refactor routing by creating AppRoutes component for cleaner structure

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import { NavLink, Route, Routes } from "react-router";
+import { NavLink } from "react-router";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   const [toggle, setToggle] = useState("Home");
@@ -18,13 +16,8 @@ const App = () => {
         </div>
         <button>Login</button>
       </nav>
-
       <div>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/contact" element={<Contact />}/>
-        </Routes>
+        <AppRoutes />
       </div>
     </div>
   );
