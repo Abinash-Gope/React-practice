1a2ecb2329e0a68f24dac82e237295737787f55e
Abinash Gope <abinashg797@gmail.com>
2026-08-02 23:20:06 +0530

    feat: replace static navigation links with NavLink components for routing

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import { Route, Routes } from "react-router";
+import { NavLink, Route, Routes } from "react-router";
 
 const App = () => {
   const [toggle, setToggle] = useState("Home");
@@ -12,9 +12,9 @@ const App = () => {
       <nav className="flex items-center justify-between mb-4">
         <h1>Logo</h1>
         <div className="flex gap-10 items-center justify-between">
-          <p>Home</p>
-          <p>About</p>
-          <p>Contact</p>
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/about"}>About</NavLink>
+          <NavLink to={"/contact"}>Contact</NavLink>
         </div>
         <button>Login</button>
       </nav>
