d514bb08e45ad2a5e2068ea4979e2e9eb1d6372e
Abinash Gope <abinashg797@gmail.com>
2026-08-02 23:34:20 +0530

    feat: extract Navbar component for improved code organization

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router";
 import AppRoutes from "./routes/AppRoutes";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const [toggle, setToggle] = useState("Home");
 
   return (
     <div className="h-screen p-2">
-      <nav className="flex items-center justify-between mb-4">
-        <h1>Logo</h1>
-        <div className="flex gap-10 items-center justify-between">
-          <NavLink to={"/"}>Home</NavLink>
-          <NavLink to={"/about"}>About</NavLink>
-          <NavLink to={"/contact"}>Contact</NavLink>
-        </div>
-        <button>Login</button>
-      </nav>
+      <Navbar />
       <div>
         <AppRoutes />
       </div>
