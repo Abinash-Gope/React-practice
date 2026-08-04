08656e0ba641f719feb3b499ce9054900dd6cc51
Abinash Gope <abinashg797@gmail.com>
2026-08-04 23:33:02 +0530

    feat: add axios for API calls and implement context for product data management

--- dynamic-routing/src/pages/Home.jsx
+++ dynamic-routing/src/pages/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import axios from "axios";
+import React, { useContext } from "react";
+import { MyStore } from "../context/MyContext";
 
 const Home = () => {
+  let { productsData, setProductsData } = useContext(MyStore);
+  let getProductsData = async () => {
+    try {
+      let res = await axios.get("https://fakestoreapi.com/products");
+      console.log(res);
+    } catch (error) {
+      console.log("error in api", error);
+    }
+  };
+
+  getProductsData();
+
   return (
     <div>
       <h1>Home pages</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
