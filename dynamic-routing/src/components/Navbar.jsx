e863fefd4066cd52b0bd2f55a97f6c73edbb9fb5
Abinash Gope <abinashg797@gmail.com>
2026-08-04 22:37:03 +0530

    feat: implement routing with Navbar and update App component structure

--- dynamic-routing/src/components/Navbar.jsx
+++ dynamic-routing/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
     <div className="h-10 text-white bg-black p-5 rounded flex items-center justify-between">
       <div>logo</div>
       <div className="flex gap-10 text-xl">
-        <p className="cursor-pointer">Home</p>
-        <p className="cursor-pointer">About</p>
-        <p className="cursor-pointer">Products</p>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+        <NavLink to={"/products"}>Products</NavLink>
       </div>
       <button>Login</button>
     </div>
