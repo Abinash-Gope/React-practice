786b1f69a6bb39c94aeabfb8471550c55cfa3e28
Abinash Gope <abinashg797@gmail.com>
2026-08-17 20:55:34 +0530

    feat: add axios for API calls and implement user data fetching in UsersPage component

--- project_interceptors/src/pages/UsersPage.jsx
+++ project_interceptors/src/pages/UsersPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
-}
 
-export default UsersPage
+  const [usersData, setUsersData] = useState([])
+  let getUsersData = async () => {
+    try {
+      let res = await axios.get("https://fakestoreapi.com/users");
+      console.log(res);
+      setUsersData(res.data)
+    } catch (error) {
+      console.log("error in user api", error);
+    }
+  };
+
+  useEffect(()=>{
+    getUsersData();
+  }, [])
+
+  return <div>UsersPage</div>;
+};
+
+export default UsersPage;
