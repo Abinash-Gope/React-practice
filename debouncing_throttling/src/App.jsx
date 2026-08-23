09882d6e3c849ba236b131209b3bc49712da25c7
Abinash Gope <abinashg797@gmail.com>
2026-08-23 23:12:42 +0530

    feat: implement product fetching and search functionality in App component

--- debouncing_throttling/src/App.jsx
+++ debouncing_throttling/src/App.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
+  const [searchData, setSearchData] = useState(null);
+  const [productsData, setProductsData] = useState([]);
+
   let getProducts = async () => {
-    let res = await axios.get("https://fakestoreapi.com/products");
+    try {
+      let res = await axios.get("https://fakestoreapi.com/products");
+      setProductsData(res.data);
+    } catch (error) {
+      console.log("error in the product api", error);
+    }
   };
 
-  return <div>App</div>;
+  let filterData = () => {
+    let result = productsData.filter((val) => {
+      return val.title === searchData;
+    });
+    console.log(result);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Debouncing...</h1>
+
+      <input
+        style={{ padding: "10px 30px" }}
+        type="text"
+        placeholder="Search Products.."
+        onChange={(e) => setSearchData(e.target.value)}
+      />
+
+      {productsData.map((val) => {
+        return <h3 key={val.id}>{val.title}</h3>;
+      })}
+    </div>
+  );
 };
 
 export default App;
