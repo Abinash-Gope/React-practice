cc58dfbb82649eb86d38d463fc7f53eb2422dfd3
Abinash Gope <abinashg797@gmail.com>
2026-08-24 22:56:04 +0530

    feat: enhance MainLayout with navigation links and lazy loading for About and Contact pages

--- debouncing_throttling/src/layouts/MainLayout.jsx
+++ debouncing_throttling/src/layouts/MainLayout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
     <div>
-      <nav>Navbar</nav>
+      <nav style={{display:"flex", gap:"30px", fontSize:"20px"}}>
+        <NavLink to={"/"}>App</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+        <NavLink to={"/contact"}>Contact</NavLink>
+      </nav>
       <Outlet />
     </div>
   );
