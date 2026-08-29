363a4662988470e560f4a6e1ad784bcf3348c69f
Abinash Gope <abinashg797@gmail.com>
2026-08-30 00:56:25 +0530

    feat: implement Redux store and counter functionality with increment/decrement actions

--- authentication_redux/src/pages/HomePage.jsx
+++ authentication_redux/src/pages/HomePage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { store } from "../app/store";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "../features/counterSlice";
 
 const HomePage = () => {
+  const [inpValue, setInpValue] = useState(0);
+  let dispatch = useDispatch();
+  let { count } = useSelector((store) => store.counter);
   return (
     <div>
-        <h1>Home page</h1>
+      <h1>Count is {count}</h1>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <br />
+      <br />
+      <input
+        type="number"
+        placeholder="Enter count"
+        onChange={(e) => setInpValue(e.target.value)}
+      />
+      <button onClick={() => dispatch(incrementByValue(inpValue))}>
+        Add to count
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
